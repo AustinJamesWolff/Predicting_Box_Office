a708608e66e13891c322718f10e4963ca1030a7f
AustinJamesWolff <austinwolff1997@gmail.com>
2021-11-12 02:55:32 -0800

    Imported model

--- pages/predictions.py
+++ pages/predictions.py
@@ -4,9 +4,13 @@ import dash_bootstrap_components as dbc
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
+from joblib import load
 
 # Imports from this application
 from app import app
+
+# import model_xgb_2 for predictions
+model_xgb_2 = load('assets/model_xgb_2.joblib')
 
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
