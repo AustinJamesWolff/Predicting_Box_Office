aac1592eeeb132257f95e8a7f4a011353df4baa0
AustinJamesWolff <austinwolff1997@gmail.com>
2021-11-13 16:21:02 -0800

    commit

--- pages/index.py
+++ pages/index.py
@@ -20,60 +20,64 @@ model_xgb_4 = load('assets/model_xgb_4.joblib') # NO Total_Gross_Bankability, NO
 
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
-row1 = dbc.Row(
+
+row = html.Div(
     [
-        dcc.Markdown(
-            """
-        
-            ## Estimate Your Movie's Box Office Using AI
+        row1 = dbc.Row(
+            [
+                dcc.Markdown(
+                    """
+                
+                    ## Estimate Your Movie's Box Office Using AI
 
-            Simply enter your movie's details (like budget) and
-            get a rough estimate of what your box office might be.
+                    Simply enter your movie's details (like budget) and
+                    get a rough estimate of what your box office might be.
 
-            """
-        )
-        
-    ],
-    md=4,
-)
-
-# gapminder = px.data.gapminder()
-# fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-#            hover_name="country", log_x=True, size_max=60)
-
-# column2 = dbc.Col(
-#     [
-#         dcc.Graph(figure=fig),
-#     ]
-# )
-
-row2 = dbc.Row(
-    [
-        dcc.Markdown("## Enter Your Movie's Details", className='mb-5'),
-        dcc.Markdown("#### Genre: (Select One)"),
-        dcc.Dropdown(
-            id='genre',
-            options = [
-                {'label':'Comedy', 'value':'Comedy'},
-                {'label' : 'Action', 'value' : 'Action'},
-                {'label' : 'Drama', 'value' : 'Drama'},
-                {'label' : 'Crime', 'value': 'Crime'},
-                {'label' : 'Biography', 'value': 'Biography'},
-                {'label' : 'Adventure', 'value' : 'Adventure'},
-                {'label' : 'Animation', 'value' : 'Animation'},
-                {'label' : 'Horror', 'value' : 'Horror'},
-                {'label' : 'Fantasy', 'value' : 'Fantasy'},
-                {'label' : 'Mystery', 'value' : 'Mystery'},
+                    """
+                )
+                
             ],
-            value="Comedy",
-            className='mb-5'),
-        dcc.Markdown("#### Budget: (Please Enter A Number)"),
-        dcc.Input(value='budget', type='text')
+            md=4,
+        )
+    ],
+        # gapminder = px.data.gapminder()
+        # fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
+        #            hover_name="country", log_x=True, size_max=60)
+
+        # column2 = dbc.Col(
+        #     [
+        #         dcc.Graph(figure=fig),
+        #     ]
+        # )
+    [
+        row2 = dbc.Row(
+            [
+                dcc.Markdown("## Enter Your Movie's Details", className='mb-5'),
+                dcc.Markdown("#### Genre: (Select One)"),
+                dcc.Dropdown(
+                    id='genre',
+                    options = [
+                        {'label':'Comedy', 'value':'Comedy'},
+                        {'label' : 'Action', 'value' : 'Action'},
+                        {'label' : 'Drama', 'value' : 'Drama'},
+                        {'label' : 'Crime', 'value': 'Crime'},
+                        {'label' : 'Biography', 'value': 'Biography'},
+                        {'label' : 'Adventure', 'value' : 'Adventure'},
+                        {'label' : 'Animation', 'value' : 'Animation'},
+                        {'label' : 'Horror', 'value' : 'Horror'},
+                        {'label' : 'Fantasy', 'value' : 'Fantasy'},
+                        {'label' : 'Mystery', 'value' : 'Mystery'},
+                    ],
+                    value="Comedy",
+                    className='mb-5'),
+                dcc.Markdown("#### Budget: (Please Enter A Number)"),
+                dcc.Input(value='budget', type='text')
 
 
+            ]
+
+        )
     ]
-
 )
 
-
-layout = dbc.Container([row1, row2])
+# layout = dbc.Container([row1, row2])
