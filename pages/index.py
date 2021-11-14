7b14b68c1a77c89df8c8c4e4199293b793079446
AustinJamesWolff <austinwolff1997@gmail.com>
2021-11-13 16:10:08 -0800

    Added budget

--- pages/index.py
+++ pages/index.py
@@ -11,12 +11,14 @@ from joblib import load
 # Imports from this application
 from app import app
 
-model_xgb_2 = load('assets/model_xgb_2.joblib')
+# model_xgb_2 = load('assets/model_xgb_2.joblib') # Uses Total_Gross_Bankability
+# model_xgb_3 = load('assets/model_xgb_3.joblib') # NO Total_Gross_Bankability
+model_xgb_4 = load('assets/model_xgb_4.joblib') # NO Total_Gross_Bankability, NO Region
 
 
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
-column1 = dbc.Col(
+row1 = dbc.Row(
     [
         dcc.Markdown(
             """
@@ -27,8 +29,8 @@ column1 = dbc.Col(
             get a rough estimate of what your box office might be.
 
             """
-        ),
-        dcc.Link(dbc.Button("Enter Your Movie's Details", color='primary'), href='/predictions')
+        )
+        
     ],
     md=4,
 )
@@ -42,5 +44,40 @@ column1 = dbc.Col(
 #         dcc.Graph(figure=fig),
 #     ]
 # )
+
+row2 = dbc.Row(
+    [
+        dcc.Markdown("## Enter Your Movie's Details", className='mb-5'),
+        dcc.Markdown("#### Genre: (Select One)"),
+        dcc.Dropdown(
+            id='genre',
+            options = [
+                {'label':'Comedy', 'value':'Comedy'},
+                {'label' : 'Action', 'value' : 'Action'},
+                {'label' : 'Drama', 'value' : 'Drama'},
+                {'label' : 'Crime', 'value': 'Crime'},
+                {'label' : 'Biography', 'value': 'Biography'},
+                {'label' : 'Adventure', 'value' : 'Adventure'},
+                {'label' : 'Animation', 'value' : 'Animation'},
+                {'label' : 'Horror', 'value' : 'Horror'},
+                {'label' : 'Fantasy', 'value' : 'Fantasy'},
+                {'label' : 'Mystery', 'value' : 'Mystery'},
+            ],
+            value="Comedy",
+            className='mb-5'),
+        dcc.Markdown("#### Budget: (Please Enter A Number)"),
+        dcc.Input(value='budget', type='text')
+
+
+    ]
+
+)
+
+
+
+
+
+
+
 
 # layout = dbc.Row([column1, column2])
