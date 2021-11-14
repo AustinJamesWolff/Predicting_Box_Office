6ead176c2c81918626f38c72bf46ee94d29e162d
AustinJamesWolff <austinwolff1997@gmail.com>
2021-11-13 16:46:07 -0800

    commit

--- pages/index.py
+++ pages/index.py
@@ -76,104 +76,104 @@ model_xgb_4 = load('assets/model_xgb_4.joblib') # NO Total_Gross_Bankability, NO
 
 
 
-# column1 = dbc.Col(
-#             [
-#                 dcc.Markdown(
-#                     """
+column1 = dbc.Col(
+            [
+                dcc.Markdown(
+                    """
                 
-#                     ## Estimate Your Movie's Box Office Using AI
+                    ## Estimate Your Movie's Box Office Using AI
 
-#                     Simply enter your movie's details (like budget) and
-#                     get a rough estimate of what your box office might be.
+                    Simply enter your movie's details (like budget) and
+                    get a rough estimate of what your box office might be.
 
-#                     """
-#                 )
+                    """
+                )
                 
+            ],
+            md=4,
+        )
+
+gapminder = px.data.gapminder()
+fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
+hover_name="country", log_x=True, size_max=60)
+
+column2 = dbc.Col(
+            [
+                dcc.Graph(figure=fig),
+            ]
+        )
+
+layout = dbc.Row([column1, column2])
+
+
+
+
+
+
+
+# app = dash.Dash(__name__)
+
+# app.layout = html.Div([
+#     html.Div(children=[
+#         html.Label('Dropdown'),
+#         dcc.Dropdown(
+#             options=[
+#                 {'label': 'New York City', 'value': 'NYC'},
+#                 {'label': u'Montréal', 'value': 'MTL'},
+#                 {'label': 'San Francisco', 'value': 'SF'}
 #             ],
-#             md=4,
-#         )
+#             value='MTL'
+#         ),
 
-# gapminder = px.data.gapminder()
-# fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-# hover_name="country", log_x=True, size_max=60)
+#         html.Br(),
+#         html.Label('Multi-Select Dropdown'),
+#         dcc.Dropdown(
+#             options=[
+#                 {'label': 'New York City', 'value': 'NYC'},
+#                 {'label': u'Montréal', 'value': 'MTL'},
+#                 {'label': 'San Francisco', 'value': 'SF'}
+#             ],
+#             value=['MTL', 'SF'],
+#             multi=True
+#         ),
 
-# column2 = dbc.Col(
-#             [
-#                 dcc.Graph(figure=fig),
-#             ]
-#         )
+#         html.Br(),
+#         html.Label('Radio Items'),
+#         dcc.RadioItems(
+#             options=[
+#                 {'label': 'New York City', 'value': 'NYC'},
+#                 {'label': u'Montréal', 'value': 'MTL'},
+#                 {'label': 'San Francisco', 'value': 'SF'}
+#             ],
+#             value='MTL'
+#         ),
+#     ], style={'padding': 10, 'flex': 1}),
 
-# layout = dbc.Row([column1, column2])
+#     html.Div(children=[
+#         html.Label('Checkboxes'),
+#         dcc.Checklist(
+#             options=[
+#                 {'label': 'New York City', 'value': 'NYC'},
+#                 {'label': u'Montréal', 'value': 'MTL'},
+#                 {'label': 'San Francisco', 'value': 'SF'}
+#             ],
+#             value=['MTL', 'SF']
+#         ),
 
+#         html.Br(),
+#         html.Label('Text Input'),
+#         dcc.Input(value='MTL', type='text'),
 
+#         html.Br(),
+#         html.Label('Slider'),
+#         dcc.Slider(
+#             min=0,
+#             max=9,
+#             marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
+#             value=5,
+#         ),
+#     ], style={'padding': 10, 'flex': 1})
+# ], style={'display': 'flex', 'flex-direction': 'row'})
 
-
-
-
-
-app = dash.Dash(__name__)
-
-app.layout = html.Div([
-    html.Div(children=[
-        html.Label('Dropdown'),
-        dcc.Dropdown(
-            options=[
-                {'label': 'New York City', 'value': 'NYC'},
-                {'label': u'Montréal', 'value': 'MTL'},
-                {'label': 'San Francisco', 'value': 'SF'}
-            ],
-            value='MTL'
-        ),
-
-        html.Br(),
-        html.Label('Multi-Select Dropdown'),
-        dcc.Dropdown(
-            options=[
-                {'label': 'New York City', 'value': 'NYC'},
-                {'label': u'Montréal', 'value': 'MTL'},
-                {'label': 'San Francisco', 'value': 'SF'}
-            ],
-            value=['MTL', 'SF'],
-            multi=True
-        ),
-
-        html.Br(),
-        html.Label('Radio Items'),
-        dcc.RadioItems(
-            options=[
-                {'label': 'New York City', 'value': 'NYC'},
-                {'label': u'Montréal', 'value': 'MTL'},
-                {'label': 'San Francisco', 'value': 'SF'}
-            ],
-            value='MTL'
-        ),
-    ], style={'padding': 10, 'flex': 1}),
-
-    html.Div(children=[
-        html.Label('Checkboxes'),
-        dcc.Checklist(
-            options=[
-                {'label': 'New York City', 'value': 'NYC'},
-                {'label': u'Montréal', 'value': 'MTL'},
-                {'label': 'San Francisco', 'value': 'SF'}
-            ],
-            value=['MTL', 'SF']
-        ),
-
-        html.Br(),
-        html.Label('Text Input'),
-        dcc.Input(value='MTL', type='text'),
-
-        html.Br(),
-        html.Label('Slider'),
-        dcc.Slider(
-            min=0,
-            max=9,
-            marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
-            value=5,
-        ),
-    ], style={'padding': 10, 'flex': 1})
-], style={'display': 'flex', 'flex-direction': 'row'})
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
